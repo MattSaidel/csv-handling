5b7c092a600186e16924b901c50618847bdf7c28
Matthew Saidel <matthewsaidel@Matthews-MacBook-Pro.local>
2013-12-14 22:50:53 -0500

    fixed error in previous commit

--- sampler.py
+++ sampler.py
@@ -2,7 +2,7 @@ import csv
 import datetime
 
 ##sample large data set to extract only Obama and McCain donations
-file = csv.DictReader(open('small_sample.csv', 'r'))
+file = csv.reader(open('small_sample.csv', 'r'))
 frontrunner = ['P80003338', 'P80002801']
 extraction = []
 
